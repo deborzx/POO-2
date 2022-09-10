e0a614af0a58530e76e86f60e3d7c1b51a62fa09
Debora Oliveira <debora.ramos.2@outlook.com>
2022-09-10 17:48:29 -0300

    Adicionar arquivos de projeto.

---
+++ POO-2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Aluno a = new Aluno();
+            a.nome = "Benjamin";
+            a.email = "benjamin@hotmail.com";
+            a.senha = "123";
+            a.turno = "Matutino";
+            a.Logar();
+            a.Exibir();
+
+
+
+
+            Usuario u = new Usuario();
+            u.nome = "Felipe";
+            u.email = "felipe@hotmail.com";
+            u.senha = "123";    
+            //u.turno --> nao pode, porque o pai nao pode herdar do filho, e turno é de ALUNO
+
+
+            Zelador z = new Zelador();
+            z.email = "rodolfo@hotmai.com";
+            z.nome = "Rodolfo";
+            z.senha = "123";
+            z.Logar();
+            z.Exibir();
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ POO-2/Usuario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_2
+{
+    internal class Usuario
+    {
+
+        public string nome;
+        public string email;
+        public string senha;
+        protected string teste;
+
+        public void Logar()
+        {
+            Console.WriteLine(teste);
+            Console.WriteLine("Logando..\n");
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"email: {email}");
+            Console.WriteLine($"nome: {nome}");
+            Console.WriteLine($"senha: {senha}");
+
+        }
+    }
+
+    //herança: aluno herda de usuario
+    class Aluno : Usuario
+    {
+        public List<string> turmas = new List<string>();
+        public string turno = "Matutino";
+
+        public void Testando()
+        {
+            Console.WriteLine(teste);
+        }
+
+    }
+
+
+    class Zelador : Usuario
+    {
+        public List<string> tarefas = new List<string>();
+         
+    }
+
+
+}
